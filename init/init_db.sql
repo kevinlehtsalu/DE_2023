cf2dc30b8572f45cd111b59c4d0b460003570f73
kevinlehtsalu <Kevin.lehtsalu@gmail.com>
2023-11-21 18:51:01 +0200

    Data engineering project
    Initial commit

---
+++ init/init_db.sql
@@ -0,0 +1,63 @@
+-- Dimension Table: Authors
+CREATE TABLE IF NOT EXISTS authors (
+    author_id SERIAL PRIMARY KEY,
+    last_name VARCHAR(255),
+    first_name VARCHAR(255),
+    middle_name VARCHAR(255),
+    UNIQUE (last_name, first_name, middle_name)
+);
+
+-- Dimension Table: Categories
+CREATE TABLE IF NOT EXISTS categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Dimension Table: Versions
+CREATE TABLE IF NOT EXISTS versions (
+    version_id SERIAL PRIMARY KEY,
+    version VARCHAR(50),
+    created_date TIMESTAMP
+);
+
+-- Fact Table: Papers
+CREATE TABLE IF NOT EXISTS papers (
+    paper_id VARCHAR(50) PRIMARY KEY,
+    title TEXT NOT NULL,
+    submitter VARCHAR(255),
+    comments TEXT,
+    journal_ref TEXT,
+    doi TEXT,
+    report_no VARCHAR(255),
+    abstract TEXT,
+    update_date DATE
+);
+
+-- Junction Table for Papers and Authors Many-to-Many Relationship
+CREATE TABLE IF NOT EXISTS paper_authors (
+    paper_id VARCHAR(50),
+    author_id INT,
+    PRIMARY KEY (paper_id, author_id),
+    FOREIGN KEY (paper_id) REFERENCES papers(paper_id),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Junction Table for Papers and Categories Many-to-Many Relationship
+CREATE TABLE IF NOT EXISTS paper_categories (
+    paper_id VARCHAR(50),
+    category_id INT,
+    PRIMARY KEY (paper_id, category_id),
+    FOREIGN KEY (paper_id) REFERENCES papers(paper_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+-- Remember to add indexes for frequently queried columns for better performance, e.g.,
+CREATE INDEX IF NOT EXISTS idx_authors_last_name ON authors(last_name);
+CREATE INDEX IF NOT EXISTS idx_categories_name ON categories(category_name);
+CREATE INDEX IF NOT EXISTS idx_papers_update_date ON papers(update_date);
+
+--
+-- The paper_id in the papers table is a VARCHAR to accommodate the ID format from the JSON data.
+-- The authors_parsed from the JSON is broken down into first_name, middle_name, and last_name in the authors table.
+-- The versions table in this schema is simplified and does not include a direct relationship with the papers table, as it seems that versions are attributes of the papers, not independent entities. If the versions are to be connected to papers, you would need to include a paper_id foreign key in the versions table and establish the necessary relationships.
+-- Depending on the JSON structure and how you want to handle the categories, you might need to preprocess this data to split it into separate category names before inserting it into the categories table.
---
+++ init/load_data.py
@@ -0,0 +1,123 @@
+import json
+import psycopg2
+from psycopg2.extras import execute_values
+
+
+# Function to insert authors and return their IDs
+def insert_authors(cursor, authors_parsed):
+    author_ids = []
+    for author in authors_parsed:
+        author_tuple = tuple(author + [""] * (3 - len(author)))
+        try:
+            cursor.execute(
+                """
+                INSERT INTO authors (last_name, first_name, middle_name)
+                VALUES (%s, %s, %s)
+                ON CONFLICT (last_name, first_name, middle_name)
+                DO UPDATE SET last_name = EXCLUDED.last_name
+                RETURNING author_id;
+                """,
+                author_tuple
+            )
+            author_id = cursor.fetchone()[0]
+            author_ids.append(author_id)
+        except psycopg2.Error as e:
+            print(f"Database error occurred while inserting authors: {e}")
+            conn.rollback()
+    return author_ids
+
+
+# Function to insert categories and return their IDs
+def insert_categories(cursor, categories):
+    category_ids = []
+    for category in categories:
+        if category:  # Ensure category is not empty
+            try:
+                cursor.execute(
+                    """
+                    INSERT INTO categories (category_name)
+                    VALUES (%s)
+                    ON CONFLICT (category_name)
+                    DO NOTHING
+                    RETURNING category_id;
+                    """,
+                    (category,)
+                )
+                result = cursor.fetchone()
+                category_id = result[0] if result else None
+                if category_id:
+                    category_ids.append(category_id)
+            except psycopg2.Error as e:
+                print(f"Database error occurred while inserting categories: {e}")
+                conn.rollback()
+    return category_ids
+
+
+# Function to load data from JSON record
+def load_data(cursor, record):
+    try:
+        authors_parsed = record.get('authors_parsed', [])
+        categories = record.get('categories', '').split()
+        author_ids = insert_authors(cursor, authors_parsed)
+        category_ids = insert_categories(cursor, categories)
+
+        paper_id = record['id']
+        cursor.execute(
+            """
+            INSERT INTO papers (paper_id, title, submitter, comments, journal_ref, doi, report_no, abstract, update_date)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (paper_id) DO NOTHING;
+            """,
+            (paper_id, record.get('title'), record.get('submitter'), record.get('comments'),
+             record.get('journal-ref'), record.get('doi'), record.get('report-no'),
+             record.get('abstract'), record.get('update_date'))
+        )
+
+        for author_id in author_ids:
+            cursor.execute(
+                "INSERT INTO paper_authors (paper_id, author_id) VALUES (%s, %s) ON CONFLICT DO NOTHING;",
+                (paper_id, author_id)
+            )
+
+        for category_id in category_ids:
+            cursor.execute(
+                "INSERT INTO paper_categories (paper_id, category_id) VALUES (%s, %s) ON CONFLICT DO NOTHING;",
+                (paper_id, category_id)
+            )
+    except psycopg2.Error as e:
+        print(f"Database error occurred: {e}")
+        conn.rollback()
+
+
+# Connect to the PostgreSQL database
+try:
+    conn = psycopg2.connect(
+        dbname='arxiv',
+        user='admin',
+        password='admin',
+        host='localhost'
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+except psycopg2.Error as e:
+    print(f"Error connecting to the database: {e}")
+    exit(1)
+
+# Read and process the JSON file
+try:
+    with open('C:\\Users\\Kevin\\OneDrive\\Documents\\GitHub\\data_engineering_2023\\Project\\archive\\data.json', 'r',
+              encoding='utf-8') as file:
+        for line in file:
+            try:
+                record = json.loads(line)
+                load_data(cursor, record)
+            except json.JSONDecodeError as e:
+                print(f"Failed to parse JSON line: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+except Exception as e:
+    print(f"Error reading the file: {e}")
+
+# Close the connection
+cursor.close()
+conn.close()
