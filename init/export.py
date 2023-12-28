f88378b5069240c1e13844c14048348eabc1111b
kevinlehtsalu <Kevin.lehtsalu@gmail.com>
2023-12-28 13:34:27 +0200

    Initial commit

--- init/export.py
+++ init/export.py
@@ -1,5 +1,41 @@
+# import psycopg2
+# import csv
+#
+# # Database connection parameters
+# db_params = {
+#     'dbname': 'arxiv',
+#     'user': 'admin',
+#     'password': 'admin',
+#     'host': 'localhost'
+# }
+#
+# # Connect to the PostgreSQL database
+# conn = psycopg2.connect(**db_params)
+# cur = conn.cursor()
+#
+# # Function to export a single table
+# def export_table(table_name):
+#     query = f"SELECT * FROM {table_name}"
+#     with open(f'{table_name}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+#         csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)  # Quote all fields
+#         cur.execute(query)
+#         # Write the header
+#         csvwriter.writerow([desc[0] for desc in cur.description])
+#         # Write data rows
+#         for row in cur:
+#             formatted_row = [str(field).replace('"', '""') if field is not None else '' for field in row]
+#             csvwriter.writerow(formatted_row)
+#
+# # Export each table
+# for table in ['authors', 'categories', 'versions', 'papers', 'paper_authors', 'paper_categories']:
+#     export_table(table)
+#
+# # Close the database connection
+# cur.close()
+# conn.close()
+#############################################################################################################
 import psycopg2
-import csv
+import json
 
 # Database connection parameters
 db_params = {
@@ -13,23 +49,46 @@ db_params = {
 conn = psycopg2.connect(**db_params)
 cur = conn.cursor()
 
-# Function to export a single table
-def export_table(table_name):
-    query = f"SELECT * FROM {table_name}"
-    with open(f'{table_name}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)  # Quote all fields
-        cur.execute(query)
-        # Write the header
-        csvwriter.writerow([desc[0] for desc in cur.description])
-        # Write data rows
-        for row in cur:
-            formatted_row = [str(field).replace('"', '""') if field is not None else '' for field in row]
-            csvwriter.writerow(formatted_row)
+# Function to export nodes to JSON
+def export_nodes_to_json(table_name):
+    query = f"SELECT row_to_json(t) FROM (SELECT * FROM {table_name}) t"
+    cur.execute(query)
+    rows = cur.fetchall()
+    data = [row[0] for row in rows]
 
-# Export each table
-for table in ['authors', 'categories', 'versions', 'papers', 'paper_authors', 'paper_categories']:
-    export_table(table)
+    with open(f'{table_name}_nodes.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
+# Function to export relationships to JSON
+def export_relationships_to_json(relationship_table, start_table, end_table, start_field, end_field):
+    query = f"""
+    SELECT json_agg(t) AS data
+    FROM (
+        SELECT p.{start_field} AS start_id, a.{end_field} AS end_id
+        FROM {relationship_table}
+        INNER JOIN {start_table} p ON {relationship_table}.{start_field} = p.{start_field}
+        INNER JOIN {end_table} a ON {relationship_table}.{end_field} = a.{end_field}
+    ) t
+    """
+    cur.execute(query)
+    result = cur.fetchone()
+    data = result[0] if result[0] is not None else []
+
+    with open(f'{relationship_table}_relationships.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
+# Export nodes
+export_nodes_to_json('authors')
+export_nodes_to_json('categories')
+export_nodes_to_json('versions')
+export_nodes_to_json('papers')
+
+# Export relationships
+export_relationships_to_json('paper_authors', 'papers', 'authors', 'paper_id', 'author_id')
+export_relationships_to_json('paper_categories', 'papers', 'categories', 'paper_id', 'category_id')
 
 # Close the database connection
 cur.close()
 conn.close()
+
+
--- init/import.py
+++ init/import.py
@@ -1,89 +1,150 @@
+# from neo4j import GraphDatabase
+#
+# uri = "neo4j://localhost:7687"
+# username = "neo4j"
+# password = "123456789"
+#
+# driver = GraphDatabase.driver(uri, auth=(username, password))
+#
+# def import_data(session):
+#     # Import Authors
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///authors.csv' AS row
+#         MERGE (a:Author {author_id: toInteger(row.author_id)})
+#         ON CREATE SET a.last_name = row.last_name,
+#                        a.first_name = row.first_name,
+#                        a.middle_name = row.middle_name
+#         """)
+#     except Exception as e:
+#         print(f"Error importing authors: {e}")
+#
+#     # Import Categories
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///categories.csv' AS row
+#         MERGE (c:Category {category_id: toInteger(row.category_id)})
+#         ON CREATE SET c.category_name = row.category_name
+#         """)
+#     except Exception as e:
+#         print(f"Error importing categories: {e}")
+#
+#     # Import Papers
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///papers.csv' AS row
+#         MERGE (p:Paper {paper_id: row.paper_id})
+#         ON CREATE SET p.title = row.title,
+#                        p.submitter = row.submitter,
+#                        p.comments = row.comments,
+#                        p.journal_ref = row.journal_ref,
+#                        p.doi = row.doi,
+#                        p.report_no = row.report_no,
+#                        p.abstract = row.abstract,
+#                        p.update_date = row.update_date,
+#                        p.citation_count = toInteger(row.citation_count)
+#         """)
+#     except Exception as e:
+#         print(f"Error importing papers: {e}")
+#
+#     # Import Versions
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///versions.csv' AS row
+#         MERGE (v:Version {version_id: toInteger(row.version_id)})
+#         ON CREATE SET v.version = row.version,
+#                        v.created_date = row.created_date
+#         """)
+#     except Exception as e:
+#         print(f"Error importing versions: {e}")
+#
+#     # Create Paper-Author Relationships
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///paper_authors.csv' AS row
+#         MATCH (p:Paper {paper_id: row.paper_id})
+#         MATCH (a:Author {author_id: toInteger(row.author_id)})
+#         MERGE (p)-[:WRITTEN_BY]->(a)
+#         """)
+#     except Exception as e:
+#         print(f"Error creating paper-author relationships: {e}")
+#
+#     # Create Paper-Category Relationships
+#     try:
+#         session.run("""
+#         LOAD CSV WITH HEADERS FROM 'file:///paper_categories.csv' AS row
+#         MATCH (p:Paper {paper_id: row.paper_id})
+#         MATCH (c:Category {category_id: toInteger(row.category_id)})
+#         MERGE (p)-[:BELONGS_TO]->(c)
+#         """)
+#     except Exception as e:
+#         print(f"Error creating paper-category relationships: {e}")
+#
+# # Running the import
+# with driver.session() as session:
+#     print("Starting data import...")
+#     import_data(session)
+#     print("Data import completed.")
+#
+# driver.close()
+
+#####################################################################################################
+
 from neo4j import GraphDatabase
 
+# Neo4j connection URI and credentials
 uri = "neo4j://localhost:7687"
 username = "neo4j"
 password = "123456789"
 
+# Connect to Neo4j using the driver
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def import_data(session):
-    # Import Authors
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///authors.csv' AS row
-        MERGE (a:Author {author_id: toInteger(row.author_id)})
-        ON CREATE SET a.last_name = row.last_name, 
-                       a.first_name = row.first_name, 
-                       a.middle_name = row.middle_name
-        """)
-    except Exception as e:
-        print(f"Error importing authors: {e}")
 
-    # Import Categories
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///categories.csv' AS row
-        MERGE (c:Category {category_id: toInteger(row.category_id)})
-        ON CREATE SET c.category_name = row.category_name
-        """)
-    except Exception as e:
-        print(f"Error importing categories: {e}")
+def import_json_data(session, json_filename, label, primary_key_field):
+    file_path = f'file:///{json_filename}_nodes.json'
 
-    # Import Papers
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///papers.csv' AS row
-        MERGE (p:Paper {paper_id: row.paper_id})
-        ON CREATE SET p.title = row.title,
-                       p.submitter = row.submitter,
-                       p.comments = row.comments,
-                       p.journal_ref = row.journal_ref,
-                       p.doi = row.doi,
-                       p.report_no = row.report_no,
-                       p.abstract = row.abstract,
-                       p.update_date = row.update_date,
-                       p.citation_count = toInteger(row.citation_count)
-        """)
-    except Exception as e:
-        print(f"Error importing papers: {e}")
+    # Cypher query to load JSON data and create nodes
+    create_nodes_query = f"""
+    CALL apoc.load.json($file_path) YIELD value AS data
+    UNWIND data AS row
+    MERGE (n:{label} {{{primary_key_field}: row.{primary_key_field}}})
+    ON CREATE SET n += row
+    ON MATCH SET n += row
+    """
+    session.run(create_nodes_query, file_path=file_path)
 
-    # Import Versions
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///versions.csv' AS row
-        MERGE (v:Version {version_id: toInteger(row.version_id)})
-        ON CREATE SET v.version = row.version,
-                       v.created_date = row.created_date
-        """)
-    except Exception as e:
-        print(f"Error importing versions: {e}")
 
-    # Create Paper-Author Relationships
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///paper_authors.csv' AS row
-        MATCH (p:Paper {paper_id: row.paper_id})
-        MATCH (a:Author {author_id: toInteger(row.author_id)})
-        MERGE (p)-[:WRITTEN_BY]->(a)
-        """)
-    except Exception as e:
-        print(f"Error creating paper-author relationships: {e}")
+def create_relationships(session, json_filename, relationship_type, start_label, start_key, end_label, end_key):
+    file_path = f'file:///{json_filename}_relationships.json'
 
-    # Create Paper-Category Relationships
-    try:
-        session.run("""
-        LOAD CSV WITH HEADERS FROM 'file:///paper_categories.csv' AS row
-        MATCH (p:Paper {paper_id: row.paper_id})
-        MATCH (c:Category {category_id: toInteger(row.category_id)})
-        MERGE (p)-[:BELONGS_TO]->(c)
-        """)
-    except Exception as e:
-        print(f"Error creating paper-category relationships: {e}")
+    # Cypher query to load JSON data and create relationships
+    create_rels_query = f"""
+    CALL apoc.load.json($file_path) YIELD value AS data
+    UNWIND data AS rel
+    MATCH (start:{start_label} {{{start_key}: rel.start_id}})
+    MATCH (end:{end_label} {{{end_key}: rel.end_id}})
+    MERGE (start)-[r:{relationship_type}]->(end)
+    """
+    session.run(create_rels_query, file_path=file_path)
 
-# Running the import
+
+
+# Execute the import within a Neo4j session
 with driver.session() as session:
-    print("Starting data import...")
-    import_data(session)
-    print("Data import completed.")
+    print("Starting JSON data import...")
 
+    # Import data for nodes
+    import_json_data(session, 'authors', 'Author', 'author_id')
+    import_json_data(session, 'categories', 'Category', 'category_id')
+    import_json_data(session, 'papers', 'Paper', 'paper_id')
+    import_json_data(session, 'versions', 'Version', 'version_id')
+
+    # Create relationships
+    create_relationships(session, 'paper_authors', 'WRITTEN_BY', 'Paper', 'paper_id', 'Author', 'author_id')
+    create_relationships(session, 'paper_categories', 'BELONGS_TO', 'Paper', 'paper_id', 'Category', 'category_id')
+
+    print("JSON data import completed.")
+
+# Close the driver connection
 driver.close()
