f5e39fe1e86aac675c69704e1493d52d3a647860
kevinlehtsalu <Kevin.lehtsalu@gmail.com>
2023-12-29 22:18:22 +0200

    Initial commit

--- dags/export_dag.py
+++ dags/export_dag.py
@@ -121,7 +121,6 @@
 # export_task
 ##############################################################################################################
 import json
-import psycopg2
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
@@ -141,7 +140,7 @@ def export_data_to_json(ds, **kwargs):
         data = [row[0] for row in rows]
 
         # Write to a file within the Airflow worker's accessible directory
-        with open(f'/opt/airflow/data/{table_name}_nodes.json', 'w', encoding='utf-8') as jsonfile:
+        with open(f'/opt/airflow/dags/{table_name}_nodes.json', 'w', encoding='utf-8') as jsonfile:
             json.dump(data, jsonfile, ensure_ascii=False, indent=4)
 
     # Function to export relationships to JSON
@@ -159,7 +158,7 @@ def export_data_to_json(ds, **kwargs):
         result = cur.fetchone()
         data = result[0] if result[0] is not None else []
 
-        with open(f'/opt/airflow/data/{relationship_table}_relationships.json', 'w', encoding='utf-8') as jsonfile:
+        with open(f'/opt/airflow/dags/{relationship_table}_relationships.json', 'w', encoding='utf-8') as jsonfile:
             json.dump(data, jsonfile, ensure_ascii=False, indent=4)
 
     # Export nodes and relationships
@@ -173,17 +172,21 @@ def export_data_to_json(ds, **kwargs):
     cur.close()
     conn.close()
 
-# Define the DAG
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 12, 29),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+# Create the DAG instance
 dag = DAG(
     'export_postgres_data_to_json',
-    default_args={
-        'owner': 'airflow',
-        'start_date': datetime(2023, 12, 29),
-        'retries': 1,
-        'retry_delay': timedelta(minutes=1),
-    },
+    default_args=default_args,
     description='DAG to export data from PostgreSQL to JSON',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1),  # Set the schedule interval as needed
+    catchup=False,  # Disable catching up on historical runs
 )
 
 # Define the task using PythonOperator
@@ -194,6 +197,105 @@ export_task = PythonOperator(
     dag=dag,
 )
 
+# Set the task dependencies
 export_task
+
+##############################################################################################################
+# import json
+# import os
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime, timedelta
+# from airflow.hooks.postgres_hook import PostgresHook
+#
+#
+# def export_data_to_json(ds, **kwargs):
+#     # Define the database connection
+#     pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+#     conn = pg_hook.get_conn()
+#     cur = conn.cursor()
+#
+#     # Function to export nodes to JSON
+#     def export_nodes_to_json(table_name, cur):
+#         query = f"SELECT row_to_json(t) FROM (SELECT * FROM {table_name}) t"
+#         cur.execute(query)
+#         rows = cur.fetchall()
+#         data = [row[0] for row in rows]
+#
+#         # Define the directory and file path
+#         directory = '/opt/airflow/data'
+#         file_path = f'{directory}/{table_name}_nodes.json'
+#
+#         # Create the directory if it doesn't exist
+#         os.makedirs(directory, exist_ok=True)
+#
+#         # Write to a file within the shared data directory
+#         with open(file_path, 'w', encoding='utf-8') as jsonfile:
+#             json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+#
+#     # Function to export relationships to JSON
+#     def export_relationships_to_json(relationship_table, start_table, end_table, start_field, end_field, cur):
+#         query = f"""
+#         SELECT json_agg(t) AS data
+#         FROM (
+#             SELECT p.{start_field} AS start_id, a.{end_field} AS end_id
+#             FROM {relationship_table}
+#             INNER JOIN {start_table} p ON {relationship_table}.{start_field} = p.{start_field}
+#             INNER JOIN {end_table} a ON {relationship_table}.{end_field} = a.{end_field}
+#         ) t
+#         """
+#         cur.execute(query)
+#         result = cur.fetchone()
+#         data = result[0] if result[0] is not None else []
+#
+#         # Define the directory and file path
+#         directory = '/opt/airflow/data'
+#         file_path = f'{directory}/{relationship_table}_relationships.json'
+#
+#         # Create the directory if it doesn't exist
+#         os.makedirs(directory, exist_ok=True)
+#
+#         # Write to a file within the shared data directory
+#         with open(file_path, 'w', encoding='utf-8') as jsonfile:
+#             json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+#
+#     # Export nodes and relationships
+#     for table in ['authors', 'categories', 'versions', 'papers']:
+#         export_nodes_to_json(table, cur)
+#
+#     export_relationships_to_json('paper_authors', 'papers', 'authors', 'paper_id', 'author_id', cur)
+#     export_relationships_to_json('paper_categories', 'papers', 'categories', 'paper_id', 'category_id', cur)
+#
+#     # Close the database connection
+#     cur.close()
+#     conn.close()
+#
+#
+# # Define the default arguments for the DAG
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 12, 29),
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=1),
+# }
+#
+# # Create the DAG instance
+# dag = DAG(
+#     'export_postgres_data_to_json',
+#     default_args=default_args,
+#     description='DAG to export data from PostgreSQL to JSON',
+#     schedule_interval=timedelta(days=1),
+#     catchup=False,
+# )
+#
+# # Define the task using PythonOperator
+# export_task = PythonOperator(
+#     task_id='export_data',
+#     python_callable=export_data_to_json,
+#     dag=dag,
+# )
+#
+# # Set the task dependencies
+# export_task
 
 
--- init/import.py
+++ init/import.py
@@ -149,61 +149,88 @@
 # # Close the driver connection
 # driver.close()
 #######################################################################################################################
-
-
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 from neo4j import GraphDatabase
 
-# Neo4j connection URI and credentials
-uri = "neo4j://localhost:7687"
-username = "neo4j"
-password = "123456789"
+# These are the DAG's default arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 12, 29),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
 
-# Connect to Neo4j using the driver
-driver = GraphDatabase.driver(uri, auth=(username, password))
+# This is the DAG definition
+dag = DAG(
+    'import_data_to_neo4j',
+    default_args=default_args,
+    description='Import JSON data into Neo4j',
+    schedule_interval=None,  # Manual trigger
+    catchup=False
+)
 
-def import_json_data(session, json_filename, label, primary_key_field):
-    file_path = f'file:///import/{json_filename}_nodes.json'  # Updated file path
+# This function will create a Neo4j session and import the JSON data
+def import_data_to_neo4j():
+    uri = "neo4j://my_neo4j:7687"  # Use the container name as the hostname
+    username = "neo4j"
+    password = "123456789"
 
-    # Cypher query to load JSON data and create nodes
-    create_nodes_query = f"""
-    CALL apoc.load.json($file_path) YIELD value AS data
-    UNWIND data AS row
-    MERGE (n:{label} {{{primary_key_field}: row.{primary_key_field}}})
-    ON CREATE SET n += row
-    ON MATCH SET n += row
-    """
-    session.run(create_nodes_query, file_path=file_path)
+    # Connect to Neo4j using the driver
+    driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def create_relationships(session, json_filename, relationship_type, start_label, start_key, end_label, end_key):
-    file_path = f'file:///import/{json_filename}_relationships.json'  # Updated file path
+    with driver.session() as session:
+        # Define the paths to the JSON files
+        node_files = {
+            'authors': 'Author',
+            'categories': 'Category',
+            'papers': 'Paper',
+            'versions': 'Version'
+        }
+        rel_files = {
+            'paper_authors': ('WRITTEN_BY', 'Paper', 'paper_id', 'Author', 'author_id'),
+            'paper_categories': ('BELONGS_TO', 'Paper', 'paper_id', 'Category', 'category_id')
+        }
 
-    # Cypher query to load JSON data and create relationships
-    create_rels_query = f"""
-    CALL apoc.load.json($file_path) YIELD value AS data
-    UNWIND data AS rel
-    MATCH (start:{start_label} {{{start_key}: rel.start_id}})
-    MATCH (end:{end_label} {{{end_key}: rel.end_id}})
-    MERGE (start)-[r:{relationship_type}]->(end)
-    """
-    session.run(create_rels_query, file_path=file_path)
+        # Import nodes
+        for json_filename, label in node_files.items():
+            file_path = f'/var/lib/neo4j/import/{json_filename}_nodes.json'  # Path inside the Neo4j container
+            create_nodes_query = """
+            CALL apoc.load.json($file_path) YIELD value AS data
+            UNWIND data AS row
+            MERGE (n:{label} {{ {primary_key_field}: row.{primary_key_field} }})
+            ON CREATE SET n += row
+            ON MATCH SET n += row
+            """
+            session.run(create_nodes_query, parameters={'file_path': file_path, 'label': label, 'primary_key_field': 'id'})
 
-# Execute the import within a Neo4j session
-with driver.session() as session:
-    print("Starting JSON data import...")
+        # Create relationships
+        for json_filename, (relationship_type, start_label, start_key, end_label, end_key) in rel_files.items():
+            file_path = f'/var/lib/neo4j/import/{json_filename}_relationships.json'  # Path inside the Neo4j container
+            create_rels_query = """
+            CALL apoc.load.json($file_path) YIELD value AS data
+            UNWIND data AS rel
+            MATCH (start:{start_label} {{ {start_key}: rel.start_id }})
+            MATCH (end:{end_label} {{ {end_key}: rel.end_id }})
+            MERGE (start)-[r:{relationship_type}]->(end)
+            """
+            session.run(create_rels_query, parameters={'file_path': file_path, 'relationship_type': relationship_type, 'start_label': start_label, 'start_key': start_key, 'end_label': end_label, 'end_key': end_key})
 
-    # Import data for nodes
-    import_json_data(session, 'authors', 'Author', 'author_id')
-    import_json_data(session, 'categories', 'Category', 'category_id')
-    import_json_data(session, 'papers', 'Paper', 'paper_id')
-    import_json_data(session, 'versions', 'Version', 'version_id')
+        print("JSON data import completed.")
 
-    # Create relationships
-    create_relationships(session, 'paper_authors', 'WRITTEN_BY', 'Paper', 'paper_id', 'Author', 'author_id')
-    create_relationships(session, 'paper_categories', 'BELONGS_TO', 'Paper', 'paper_id', 'Category', 'category_id')
+    # Close the driver connection
+    driver.close()
 
-    print("JSON data import completed.")
+# Define the PythonOperator to execute the import_data_to_neo4j function
+import_to_neo4j_task = PythonOperator(
+    task_id='import_to_neo4j',
+    python_callable=import_data_to_neo4j,
+    dag=dag
+)
 
-# Close the driver connection
-driver.close()
+# Set the task dependencies
+import_to_neo4j_task
+
 
 
